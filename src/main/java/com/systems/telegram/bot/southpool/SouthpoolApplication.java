9282bb43cb350f6b453e9a913ddd97bfb7754536
Oliver Lundag <oliver@lapapa88.com>
2019-01-22 12:25:12 +0900

    Initial commit

---
+++ src/main/java/com/systems/telegram/bot/southpool/SouthpoolApplication.java
@@ -0,0 +1,14 @@
+package com.systems.telegram.bot.southpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthpoolApplication.class, args);
+	}
+
+}
+
